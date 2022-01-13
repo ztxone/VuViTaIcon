0c20d9de86a56d3d15c5a27bc98479fce084695d
Anton Cherry <39146381+ztxone@users.noreply.github.com>
2022-01-13 20:26:16 +0200

    init

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# VuViTaIcon
- VuViTaIcon - Template starter with Vite, Vue3, TailwindCss3, Iconify
+# Built with Vue 3 + Vite + TailwindCss3 + Iconify
+
+This template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
+
+## Recommended IDE Setup
+
+- [VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=johnsoncodehk.volar)
+
+## Use
+
+1. Clone repo
+2. npm i
+3. npm run dev
---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="flex w-full h-full justify-center items-center">
+    <HelloWorld msg="Hello Vue 3 + Vite" />
+  </div>
+  
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
